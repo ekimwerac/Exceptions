23b4797d70d04f7b29f34a1abbb35fde4c3439ed
Mike Carew <mike@carew.uk>
2023-11-08 22:37:29 +0000

    Notes and Exception initializer using when with Exception properties

--- Program.cs
+++ Program.cs
@@ -1,26 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//In this example:
+// 1. We define a custom exception class MyException, which inherits from Exception and includes a boolean property MinorFault.
+// 2. We use an exception initializer when throwing a MyException, setting the MinorFault property to true.
+// 3. In the catch block, we use the when clause to filter exceptions. The first catch block catches MyException instances where MinorFault is true, and the second catch block catches instances where MinorFault is false. The messages displayed in each catch block depend on the value of the MinorFault property.
+// 4. If no exception matches the conditions in the first two catch blocks, the third catch block catches any other exceptions and displays a general message.
+// 5. This allows you to use the MinorFault property to distinguish and handle exceptions differently based on their custom properties.
 using System;
 
-public class CustomException : Exception
+public class MyException : Exception
 {
-    public DateTime Timestamp { get; }
-    public string AdditionalInfo { get; }
+    public bool MinorFault { get; set; }
 
-    public CustomException(string message, DateTime timestamp, string additionalInfo)
-        : base(message)
+    public MyException() : base("Custom Exception")
     {
-        Timestamp = timestamp;
-        AdditionalInfo = additionalInfo;
+        MinorFault = false;
     }
-
-    public CustomException(string message, Exception innerException, DateTime timestamp, string additionalInfo)
-        : base(message, innerException)
-    {
-        Timestamp = timestamp;
-        AdditionalInfo = additionalInfo;
-    }
-
-    // You can add more constructors and properties as needed.
 }
 
 class Program
@@ -29,24 +23,23 @@ class Program
     {
         try
         {
-            // Simulate an inner exception
-            var innerException = new InvalidOperationException("Inner exception occurred");
-            throw new CustomException("Custom exception occurred", innerException, DateTime.Now, "Some additional information");
+            // Simulate throwing a MyException with MinorFault set to true
+            throw new MyException() { MinorFault = true };
         }
-        catch (CustomException ex)
+        catch (MyException ex) when (ex.MinorFault)
         {
-            Console.WriteLine("Custom Exception Details:");
-            Console.WriteLine($"Message: {ex.Message}");
-            Console.WriteLine($"Timestamp: {ex.Timestamp}");
-            Console.WriteLine($"Additional Info: {ex.AdditionalInfo}");
-
-            if (ex.InnerException != null)
-            {
-                Console.WriteLine("Inner Exception Details:");
-                Console.WriteLine($"Message: {ex.InnerException.Message}");
-                // You can access additional properties of the inner exception here.
-            }
+            Console.WriteLine("Caught a MyException with MinorFault set to true");
+            Console.WriteLine($"Exception Message: {ex.Message}");
+        }
+        catch (MyException ex) when (!ex.MinorFault)
+        {
+            Console.WriteLine("Caught a MyException with MinorFault set to false");
+            Console.WriteLine($"Exception Message: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Caught a general exception");
+            Console.WriteLine($"Exception Message: {ex.Message}");
         }
     }
 }
-
